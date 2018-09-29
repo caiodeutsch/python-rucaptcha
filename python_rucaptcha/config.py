6a6af8e27eeabe63e1eb359f38499e467ed83e86
Andrei <drang.andray@gmail.com>
2018-09-29 03:03:05 +0300

    Удаление лишних файлов с бэкэнда сайта.

--- python_rucaptcha/config.py
+++
@@ -1,6 +0,0 @@
-# Сервер для отправки данных
-url_request = "http://2captcha.com/in.php"
-# Сервер для получения ответа
-url_response = "http://2captcha.com/res.php"
-# ключ приложения
-app_key = "1899"
